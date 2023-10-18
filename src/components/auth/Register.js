bea72dac0a95d4aa0c48bc59b92c23a125fc7e13
cefer <cefer@DESKTOP-HU1O35U>
2023-10-18 04:20:01 -0700

    modified:   src/components/auth/Register.js

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -54,7 +54,7 @@ const Register = () => {
             let regobj = { id, name, password, email, phone, country, address, gender };
             if (IsValidate()) {
             //console.log(regobj);
-            fetch("http://localhost:8000/users", {
+            fetch("http://jamespeterjacob.github.io:8000/users", {
                 method: "POST",
                 headers: { 'content-type': 'application/json' },
                 body: JSON.stringify(regobj)
